286d2999bd6738b7955bed6324275a79baa4480a
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-27 10:49:23 +0200

    Feat: Add .env

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'Super Secret',
+  secret: process.env.SESSION_SECRET || 'mellamomario',
   resave: true,
   saveUninitialized: true,
   cookie: {
