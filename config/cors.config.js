5306fca8a67966a062f93a9fdfca0641dc9b5a34
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-28 11:50:30 +0200

    Feat: Cors limited

--- config/cors.config.js
+++ config/cors.config.js
@@ -2,7 +2,8 @@
 
 const originsAllowed = process.env.CORS_ORIGINS || [
   'http://localhost:3000',
-  'http://localhost:4200'
+  'http://localhost:4200',
+  'https://wht-api.herokuapp.com/'
 ];
 
 module.exports = {
--- routes/session.routes.js
+++ routes/session.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const secureMiddleware = require('../middleware/secure.middleware');
 const sessionController = require('../controllers/session.controller');
 
-// router.delete('/', secureMiddleware.isAuthenticated, sessionController.destroy);
-router.delete('/', sessionController.destroy);
+router.delete('/', secureMiddleware.isAuthenticated, sessionController.destroy);
+// router.delete('/', sessionController.destroy);
 router.post('/', sessionController.create);
 
 module.exports = router;
