8eacccef4eaf07bde76265b4676638555f1442f0
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-28 12:36:27 +0200

    a

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors(corsConfig))
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://wht-api.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://wht-api.herokuapp.com/users');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
