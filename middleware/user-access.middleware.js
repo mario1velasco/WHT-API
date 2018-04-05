85c4631b694412dafbeccc7aabef73329dcd6bdc
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-04-05 17:18:06 +0200

    feat: tidy

--- middleware/user-access.middleware.js
+++ middleware/user-access.middleware.js
@@ -3,10 +3,7 @@ const ApiError = require('../models/api-error.model');
 module.exports.gotAccess = (req, res, next) => {
   const idParams = String(req.params.id);
   const idUser = String(req.user._id);
-  console.log("AAAAA");
-  
-
-  if (idParams===idUser) {
+  if (idParams === idUser) {
     next();
   } else {
     next(new ApiError('Unauthorized, diferents ids', 403));
