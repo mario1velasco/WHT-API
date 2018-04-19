86caf1711a8610e37d5a7ce765863391efc1a5bd
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-04-19 21:16:31 +0200

    fix: socket config

--- app.js
+++ app.js
@@ -55,11 +55,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-  res.locals.session = req.user || {};
-  next();
-});
-
 app.use('/users', usersRoutes);
 app.use('/users', chatsRoutes);
 app.use('/users', messageRoutes);
@@ -74,10 +69,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   // res.render('error');
